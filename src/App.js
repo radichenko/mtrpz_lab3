4fee6d5ab540ba7edc74cbf66632bd317289f9c5
radichenko <radichenkonazar@gmail.com>
2024-06-03 18:19:04 +0300

    Project setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Will by my 3d lab
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/radichenko/mtrpz_lab3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See at GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
